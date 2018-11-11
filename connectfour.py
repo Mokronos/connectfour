9c4d0f4ff6395c01332ca5b2868dec0b2b6bf65d
Sebastian Hirt <sebastian@hirts.de>
2018-11-11 17:50:55 +0100

    update function

--- connectfour.py
+++ connectfour.py
@@ -4,6 +4,9 @@ import numpy as np
 x=np.zeros((6,7))
 print(x)
 
+x[1,1]=5
+
+print(x[1,1])
 
 print("hello")
 
@@ -11,14 +14,23 @@ print("hello")
 def update(board,action,player):
     if not 0<=(action)<=6:
         print("action not in action space")
-        return 0
+        return 0,board
     if not 1<=player<=2:
         print("not a viable player")
-        return 0
+        return 0,board
     if not board.shape==(6,7):
         print("wrong board dimensions")
-        return 0
+        return 0,board
+    
+    if board[0,action]!=0:
+        print("wrong action_column full")
+        return 0,board
+    
+    for i in range(6):
+        if board[i,action]!=0:
+            board[i-1,action]=player
+            return 1,board
 
-# is this really saved?
+
 
 
