491edd207a762309abf2b09b3f41f6d01f6813c0
Mokronos <sebastian@hirts.de>
2018-11-22 06:06:05 +0100

    added help function, started main mcts function, commented plan for main mcts function

--- mcts.py
+++ mcts.py
@@ -39,6 +39,8 @@ def addState(data,state):
     state=np.reshape(state,(1,state.size))
     data=np.concatenate((data,state))
     return data
+
+#returns visitcount for given state
 def visitcount(data,state):
     state=state.flatten()
     datax=np.delete(data,[[data.shape[1]-1,data.shape[1]-2]],axis=1)
@@ -47,7 +49,7 @@ def visitcount(data,state):
             return data[i,data.shape[1]-2]
     return 0
 
-
+#returns value for given state
 def value(data,state):
     state=state.flatten()
     datax=np.delete(data,[[data.shape[1]-1,data.shape[1]-2]],axis=1)
@@ -56,15 +58,34 @@ def value(data,state):
             return data[i,data.shape[1]-1]
     return 0
 
+#increases visitcount by 1 for given state in given data
+def up_visitcount(data,state):
+    state=state.flatten()
+    datax=np.delete(data,[[data.shape[1]-1,data.shape[1]-2]],axis=1)
+    for i in range(datax.shape[0]):
+        if((datax[i]==state).all()):
+            data[i,data.shape[1]-2]+=1
+    
+    return
+#increases value by given value for given state in given data
+def up_value(data,state,value):
+    state=state.flatten()
+    datax=np.delete(data,[[data.shape[1]-1,data.shape[1]-2]],axis=1)
+    for i in range(datax.shape[0]):
+        if((datax[i]==state).all()):
+            data[i,data.shape[1]-1]=data[i,data.shape[1]-1]+value
 
+    return
+#returns 2*actions array with visits,values for each action(action following child)
 def collect_possible_childs(state,player):
-    childs=np.zeros((action_space,2))
+    childs=np.zeros((action_space,state.size+2))
     for i in range(action_space):
         next_state=update(state,i,player)[2]
-        childs[i,0]=visitcount(data,next_state)
-        childs[i,1]=value(data,next_state)
+        childs[i,0:state.size]=next_state.flatten()
+        childs[i,childs.shape[1]-2]=visitcount(data,next_state)
+        childs[i,childs.shape[1]-1]=value(data,next_state)
     return childs
-
+#plays out a game from given state with given player at random
 def playout(state,player):
     #w=3 ... 3 for game not finished/sync with update function
     w=3
@@ -81,12 +102,37 @@ def playout(state,player):
         
     return w
 
+def check_if_leaf(state,player):
+    childs=collect_possible_childs(state,player)
+    
+    unvisited=np.empty(1)
+    for i in range(action_space):
+        if childs[i,childs.shape1[1]-2]==0:
+            np.append(unvisited,i)
 
 
 def mcts(state,player):
+    
+    #selection: select best states according to policy(rewards/visits) until state has 1 or more unvisited children
+    #expand: select 1 random unvisited child and add it to tree with 0 value, 0 visits
+    #simulation: simulate from created unvisited child to end with random actions and get reward
+    #backpropagation: update stats of all nodes traveled down to simulation ... all visits +1 / pos. reward for all  nodes opposite of winner, neg. reward for all nodes of winner (pos reward for the states that are used to determine the best action for the winner ... which are the states in which the loser needs choose an action)
+    #track states while selecting and expanding to update easy while backpropagating
+
     childs=collect_possible_childs(state,player)
     
+    unvisited=np.empty(1)
+    for i in range(action_space):
+        if childs[i,childs.shape1[1]-2]==0:
+            np.append(unvisited,i)
 
+    sel_child=childs[unvisited[randint(0,unvisited.size)],0:childs.shape[1]-2]
+        
+    #if all(childs[,childs.shape[1]-2])==0
+    
+    #crv=current rollout value ... array with rollout values for every action
+    crv=playout(sel_child,player)
+        
     return
 
 
@@ -133,4 +179,10 @@ print(g)
 print(data2.shape[0])
 print(data2[2,36])
 
+f=np.zeros((action_space,ini.size+2))
+print(state2)
 
+f[0,0:42]=state2.flatten()
+print(f[0,0:42])
+
+print(f)
